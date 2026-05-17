6ef73d204adbdf54211d230a89e923045712e7b4
abhiydvv <abhiydvmac@gmail.com>
2026-05-17 23:53:40 +0530

    enable polling fallback

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import "./App.css";
 
 const socket = io("https://algo-backend-s750.onrender.com", {
-  transports: ["websocket", "polling"],
+  transports: ["polling", "websocket"],
 });
 
 function App() {
