8fa90f368078789dc12c7dfd9f0c8b3d72679f12
abhiydvv <abhiydvmac@gmail.com>
2026-05-17 23:07:57 +0530

    complete final working dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import "./App.css";
 
 const socket = io("https://algo-backend-s750.onrender.com", {
-  transports: ["polling"],
+  transports: ["polling", "websocket"],
   reconnection: true,
 });
 
@@ -21,6 +21,7 @@ function App() {
     });
 
     socket.on("marketData", (data) => {
+      console.log("DATA:", data);
       setStocks(data);
     });
 
