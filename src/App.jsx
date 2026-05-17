3e0f27483b911a26bf8ac274351dec94e57136db
abhiydvv <abhiydvmac@gmail.com>
2026-05-18 00:37:43 +0530

    fix vite deployment

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,34 @@ import { io } from "socket.io-client";
 import "./App.css";
 
 const socket = io("https://algo-backend-s750.onrender.com", {
-  transports: ["polling"],
+  transports: ["websocket", "polling"],
 });
+
 function App() {
   const [stocks, setStocks] = useState({
-    AAPL: { price: "Loading...", change: 0 },
-    TSLA: { price: "Loading...", change: 0 },
-    BTC: { price: "Loading...", change: 0 },
-    ETH: { price: "Loading...", change: 0 },
+    AAPL: {
+      price: "Loading...",
+      change: "0",
+      signal: "WAIT",
+    },
+
+    TSLA: {
+      price: "Loading...",
+      change: "0",
+      signal: "WAIT",
+    },
+
+    BTC: {
+      price: "Loading...",
+      change: "0",
+      signal: "WAIT",
+    },
+
+    ETH: {
+      price: "Loading...",
+      change: "0",
+      signal: "WAIT",
+    },
   });
 
   useEffect(() => {
@@ -35,16 +55,51 @@ function App() {
   return (
     <div className="app">
       <h1>Algorithmic Trading Dashboard</h1>
-      <p>Real-Time Market Overview</p>
+
+      <p className="subtitle">Real-Time Market Overview</p>
 
       <div className="cards">
+
         {Object.entries(stocks).map(([symbol, data]) => (
           <div className="card" key={symbol}>
+
             <h2>{symbol}</h2>
+
             <h1>${data.price}</h1>
-            <p>{data.change}%</p>
+
+            <p
+              className={
+                Number(data.change) >= 0 ? "positive" : "negative"
+              }
+            >
+              {data.change}%
+            </p>
+
+            <div className="actions">
+
+              <button className="buy">
+                BUY
+              </button>
+
+              <button className="sell">
+                SELL
+              </button>
+
+            </div>
+
+            <div
+              className={
+                data.signal === "BUY"
+                  ? "signal-buy"
+                  : "signal-sell"
+              }
+            >
+              Signal: {data.signal}
+            </div>
+
           </div>
         ))}
+
       </div>
     </div>
   );
